f9f415b5446eb61fef4356d9722638f40c42ee9e
Ellie White <elliewhite1420@gmail.com>
2025-06-09 15:23:48 -0400

    updating csv creator and velocity calculations

--- sycorax_search/prep_csv.py
+++ sycorax_search/prep_csv.py
@@ -14,12 +14,15 @@ import pandas as pd
 
 from astropy import units as u
 from astropy.units import cds
-from astropy.coordinates import SkyCoord, GCRS, HeliocentricTrueEcliptic
+from astropy.coordinates import SkyCoord, GCRS, HeliocentricTrueEcliptic, ITRS, GeocentricTrueEcliptic, AltAz
+from astropy.coordinates import CartesianRepresentation, CartesianDifferential
+from astropy.coordinates import EarthLocation
+from astropy.time import Time
 
-from poliastro.bodies import Sun
-from poliastro.twobody import Orbit
-from poliastro.twobody.angles import M_to_E, E_to_nu, nu_to_E
-from poliastro.frames.ecliptic import HeliocentricEclipticJ2000
+#from poliastro.bodies import Sun
+#from poliastro.twobody import Orbit
+#from poliastro.twobody.angles import M_to_E, E_to_nu, nu_to_E
+#from poliastro.frames.ecliptic import HeliocentricEclipticJ2000
 
 from velocity_calcs import measured_velocity
 from velocity_calcs import keplerian_velocity
@@ -27,6 +30,9 @@ from velocity_calcs import keplerian_velocity_xyz
 from velocity_calcs import measured_velocity_spherical
 from velocity_calcs import keplerian_velocity_spherical
 from velocity_calcs import return_nu_ap
+
+rubin_loc = EarthLocation.from_geodetic(-70.74941666666666*u.deg, \
+                                        -30.244633333333333*u.deg, 2662.75*u.m)
 
 def main():
     # import original file
@@ -36,8 +42,8 @@ def main():
     #infile_name = '/home/ellie/research/lsst/table_dp03_catalogs_10yr.MPCORB-AS-mpc-JOIN-dp03_c.csv'
     #outfile_name = '/home/ellie/research/lsst/LSST_sim.csv'
     
-    infile_name = '/home/ellie/research/lsst/s1003Hmna_data.csv' #
-    outfile_name = '/home/ellie/research/lsst/s1003Hmna_data_vel_poli.csv' #
+    infile_name = '/home/ellie/Downloads/1M_Objects.csv' #
+    outfile_name = '/home/ellie/research/lsst/1M_Objects_vel.csv' #
     
     #infile_name = '/home/ellie/research/lsst/S100a6n8a_data.csv' #s1003Hmna_data.csv' #
     #outfile_name = '/home/ellie/research/lsst/S100a6n8a_data_vel.csv' #s1003Hmna_data_vel.csv' #
@@ -95,7 +101,7 @@ def main():
     
     #print(indata['E'])   
     
-    for ind, row in indata.iterrows():
+    '''for ind, row in indata.iterrows():
         E = (nu_to_E(nu[ind], ecc[ind])) #+ 1.1*np.pi*u.rad
         E_list.append(E)
         #nu = E_to_nu(E, ecc[ind])
@@ -104,7 +110,7 @@ def main():
         obj_orbit = Orbit.from_classical(Sun, u.Quantity(a[ind], u.AU), ecc[ind], \
                                        u.Quantity(i[ind], u.rad), u.Quantity(node[ind], u.rad), \
                                        u.Quantity(peri[ind], u.rad), nu[ind], \
-                                       u.Quantity(indata['epoch'][ind], cds.MJD))
+                                       u.Quantity(indata['epoch'][ind], cds.MJD), plane='EARTH_ECLIPTIC')
                                        
         #orb_heliocentric = obj_orbit.transform_to(heliocentric_frame)
         vk_vector = obj_orbit.v
@@ -124,18 +130,50 @@ def main():
         #print(vk_vector.to(u.AU / u.day)[0])
         
     indata['E'] = E_list
-    indata['nu'] = nu
+    indata['nu'] = nu'''
             
-    # x_dotk, y_dotk, z_dotk = keplerian_velocity_xyz(indata['heliocentricDist'], a, e, i, node, peri, x, y, indata)
+    x_dotk, y_dotk, z_dotk = keplerian_velocity_xyz(indata['heliocentricDist'], a, e, i, node, peri, x, y, indata)
     
-    indata['x_dotk'] = x_dotk
-    indata['y_dotk'] = y_dotk
-    indata['z_dotk'] = z_dotk
+    '''xdot_ktopo = []
+    ydot_ktopo = [] 
+    zdot_ktopo = []
+    
+    for j in range(len(x_dotk)):
+        helio = HeliocentricTrueEcliptic(x=x[j]*u.AU, y=y[j]*u.AU, \
+                z=z[j]*u.AU, v_x=x_dotk[j]*(u.AU/u.day), v_y=y_dotk[j]*(u.AU/u.day), \
+                v_z=z_dotk[j]*(u.AU/u.day), representation_type=CartesianRepresentation, \
+                differential_type=CartesianDifferential, obstime=Time('2025-01-01'))
+                
+        topo = helio.transform_to(AltAz(obstime=Time('2025-01-01'), location=rubin_loc))
+        
+        vx = topo.velocity.d_x
+        vx_topo = vx.to(u.AU/u.day)
+        
+        vy = topo.velocity.d_y
+        vy_topo = vy.to(u.AU/u.day)
+        
+        vz = topo.velocity.d_z
+        vz_topo = vz.to(u.AU/u.day)
+        
+        xdot_ktopo.append(vx_topo.value)
+        ydot_ktopo.append(vy_topo.value) 
+        zdot_ktopo.append(vz_topo.value) 
+        
+        #vxh = helio.velocity.d_x
+        #vxhc = vxh.to(u.AU/u.day)
+        
+        #print(vxhc)
+        #print(x_dotk[j])
+        #print()'''
+    
+    indata['x_dotk'] = x_dotk #xdot_ktopo
+    indata['y_dotk'] = y_dotk #ydot_ktopo
+    indata['z_dotk'] = z_dotk #zdot_ktopo
     
     ## xyz velocity difference columns
-    '''indata['delta_x_dot'] = indata['heliocentricVX'] - indata['x_dotk']
+    indata['delta_x_dot'] = indata['heliocentricVX'] - indata['x_dotk']
     indata['delta_y_dot'] = indata['heliocentricVY'] - indata['y_dotk']
-    indata['delta_z_dot'] = indata['heliocentricVZ'] - indata['z_dotk']'''
+    indata['delta_z_dot'] = indata['heliocentricVZ'] - indata['z_dotk']
     
     ## spherical measured and keplerian velocities 
     r_dot, az_dot, el_dot = measured_velocity_spherical(indata['heliocentricVX'], indata['heliocentricVY'], indata['heliocentricVZ'])
