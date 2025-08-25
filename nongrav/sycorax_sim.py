c822cbbd158bf5ba7848b69b32d8447e0f978e24
Ellie White <elliewhite1420@gmail.com>
2025-08-25 00:17:59 +0000

    tweaked the object simulation code

--- nongrav/sycorax_sim.py
+++ nongrav/sycorax_sim.py
@@ -6,7 +6,9 @@
 
 import numpy as np
 import pylab as plt
+import math
 import random
+
 from datetime import datetime
 import rebound
 import assist
@@ -26,15 +28,15 @@ def create_3d_vector(magnitude):
 
     return [x, y, z]
     
-def gen_starting_values():
+def gen_starting_values(n):
 
     ## this function will allow us to generate
     ## locations, velocities, and acceleration 
     ## parameters that we can use to initialize
     ## the ASSIST integrations
     
-    ## how many sycoraxes (sycori? sycoraxi?) do you want to generate?
-    n = 100
+    ## how many sycoraxes (sycori? sycoraxi?) you want
+    ## to generate are specified by n
     
     ## set limits on locations
     min_dist = 0.00 ## AU
@@ -87,56 +89,65 @@ def sim_ephemerides():
     min_int = 10 ## days
     max_int = 3650 ## days
     
+    ## define how many sycoraxes you want to generate
+    n = 10
+    
     ## get the arrays of vectors
-    dist_vectors, v_vectors, a_vectors = gen_starting_values()
+    dist_vectors, v_vectors, a_vectors = gen_starting_values(n)
     
     ## simulate each ephemeris
-    
-    ## note I will probably need to adjust the path to the
-    ## files below
-    ephem = assist.Ephem("../data/linux_p1550p2650.440", "../data/sb441-n16.bsp")
+    ephem = assist.Ephem("/home/ellie/src/assist/data/linux_p1550p2650.440", "/home/ellie/src/assist/data/sb441-n16.bsp")
     
     ## https://assist.readthedocs.io/en/latest/jupyter_examples/GettingStarted/ 
     
     for i in range(len(dist_vectors)):
     
+        print('simulating object {}'.format(i))
         ## create the rebound simulation and attach ASSIST
         sim = rebound.Simulation()
         ex = assist.Extras(sim, ephem)
     
         ## add each of our generated objects to the simulation
-        sycorax_initial_helio = sim.add(x = dist_vectors[0], y=dist_vectors[1], z=dist_vectors[2], \
-                                        vx = v_vectors[0], vy=v_vectors[1], vz=v_vectors[2])  
+        sycorax_initial_helio = rebound.Particle(x = dist_vectors[i][0], \
+                                                 y=dist_vectors[i][1], \
+                                                 z=dist_vectors[i][2], \
+                                                 vx = v_vectors[i][0], \
+                                                 vy=v_vectors[i][1], \
+                                                 vz=v_vectors[i][2])  
                 
         ## assign a start date to each object  
-        start_date = gen_random_date(2026, 1, 1, 2030, 12, 31)
+        start_date = gen_random_date(2015, 1, 1, 2020, 1, 1)
         t_initial = start_date - ephem.jd_ref
         
         ## convert from heliocentric to barycentric
         sun_initial = ephem.get_particle("sun", t_initial)
         sycorax_initial = sycorax_initial_helio + sun_initial
 
-        ##        
-        sim.ri_ias15.min_dt = 0.001
+        ##      
+        sim = rebound.Simulation()
+        sim.add(sycorax_initial)
+        sim.t = t_initial   
+        sim.ri_ias15.min_dt = 1
         extras = assist.Extras(sim, ephem)
         
         ## set up the acceleration parameters
-        extras.particle_params = np.array([a_vectors[0], a_vectors[1], a_vectors[2]])
+        extras.particle_params = np.array([a_vectors[i][0], a_vectors[i][1], a_vectors[i][2]])
         
         ## create the ephemeris
         int_days = random.randint(min_int, max_int)
         t_final = t_initial + int_days  - ephem.jd_ref
         
-        N_samples = int_days*3 ## this is like saying you observe it 3x per day
+        N_samples = 100 ##int_days*3 ## this is like saying you observe it 3x per day
         times = np.linspace(t_initial, t_final, N_samples, endpoint=True)
         
         sycorax_pos = np.zeros((N_samples, 3))
         earth_pos = np.zeros((N_samples, 3))
         
-        for i, t in enumerate(times):
+        for idx, t in enumerate(times):
+            print('integrating object {}, iteration {}/{}'.format(i, idx, N_samples))
             extras.integrate_or_interpolate(t)
-            sycorax_pos[i] = sim.particles[0].xyz
-            earth_pos[i] = ephem.get_particle("earth", t).xyz
+            sycorax_pos[idx] = sim.particles[0].xyz
+            earth_pos[idx] = ephem.get_particle("earth", t).xyz
             
         plt.xlabel("x [AU]")
         plt.ylabel("y [AU]")
